9a366aeed741dddfe05d092c922ca183ba24a92b
Rayhan72-max <raypatwary72@gmail.com>
2020-09-28 14:35:02 +0600

    Updates

---
+++ precache-manifest.040a11cd9d0634a41bafa406c9ca2731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759666cac920f9877c1d0b6c5903f933",
+    "url": "/Chill/index.html"
+  },
+  {
+    "revision": "ef91be754375c72653ec",
+    "url": "/Chill/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a16421c4a8f4fc0f1d3",
+    "url": "/Chill/static/js/2.906898ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chill/static/js/2.906898ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef91be754375c72653ec",
+    "url": "/Chill/static/js/main.66c701ee.chunk.js"
+  },
+  {
+    "revision": "2076fa8738094c0264bf",
+    "url": "/Chill/static/js/runtime-main.5163b2c9.js"
+  }
+]);
